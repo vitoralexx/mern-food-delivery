e0482595d8102af7c41e20b3866fde052797d1d4
vitoralexx <vitoralexse@gmail.com>
2025-03-11 14:32:57 -0300

    first commt

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order Your Favorite Food Here</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, provident. Deserunt repellat odit voluptatum eius debitis quae. Dolorum adipisci, id in illo optio consectetur natus recusandae aliquid atque molestias pariatur?</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
