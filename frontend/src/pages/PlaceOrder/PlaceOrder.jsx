cbafcd4e775c0a74b27e190b30893748426af808
vitoralexx <vitoralexse@gmail.com>
2025-04-09 15:12:10 -0300

    Final commit: completed fullstack MERN food delivery app

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,32 +1,86 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./PlaceOrder.css";
 import { StoreContext } from "../../context/StoreContext";
-import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+
 
 const PlaceOrder = () => {
 
-  const { getTotalCartAmount } = useContext(StoreContext);
+  const { getTotalCartAmount, token, food_list, cartItems, url } = useContext(StoreContext);
+
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: ""
+  })
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data=>({...data,[name]:value}))
+  }
+
+  const placeOrder = async (event) => {
+    event.preventDefault();
+    let orderItems = [];
+    food_list.map((item)=>{
+      if(cartItems[item._id] > 0) {
+        let itemInfo = item;
+        itemInfo["quantity"] = cartItems[item._id];
+        orderItems.push(itemInfo);
+      }
+    })
+    let orderData = {
+      address: data,
+      items: orderItems,
+      amount: getTotalCartAmount() + 2,
+    }
+    let response = await axios.post(url + "/api/order/place", orderData, {header:{token}})
+    if(response.data.success) {
+      const {session_url} = response.data;
+      window.location.replace(session_url);
+    }
+    else {
+      alert("Error");
+    }
+  }
+
   const navigate = useNavigate();
 
+  useEffect(()=>{
+    if(!token) {
+      navigate('/cart');
+    } else if(getTotalCartAmount() === 0) {
+      navigate('/cart');
+    }
+  },[token])
+
   return (
-    <form className="place-order">
+    <form onSubmit={placeOrder} className="place-order">
       <div className="place-order-left">
         <p className="title">Delivery Information</p>
         <div className="multi-fields">
-          <input type="text" placeholder="First Name" />
-          <input type="text" placeholder="Last Name" />
+          <input name="firstName" onChange={onChangeHandler} value={data.firstName} type="text" placeholder="First Name" required />
+          <input name="lastName" onChange={onChangeHandler} value={data.lastName} type="text" placeholder="Last Name" required />
         </div>
-        <input type="email" placeholder="Email address" />
-        <input type="text" placeholder="Street" />
+        <input name="email" onChange={onChangeHandler} value={data.email} type="email" placeholder="Email address" required />
+        <input name="street" onChange={onChangeHandler} value={data.street} type="text" placeholder="Street" required />
         <div className="multi-fields">
-          <input type="text" placeholder="City" />
-          <input type="text" placeholder="State" />
+          <input name="city" onChange={onChangeHandler} value={data.city} type="text" placeholder="City" required />
+          <input name="state" onChange={onChangeHandler} value={data.state} type="text" placeholder="State" required />
         </div>
         <div className="multi-fields">
-          <input type="text" placeholder="Zip code" />
-          <input type="text" placeholder="Country" />
+          <input name="zipcode" onChange={onChangeHandler} value={data.zipcode} type="text" placeholder="Zip code" required />
+          <input name="country" onChange={onChangeHandler} value={data.country} type="text" placeholder="Country" required />
         </div>
-        <input type="text" placeholder="Phone" />
+        <input name="phone" onChange={onChangeHandler} value={data.phone} type="text" placeholder="Phone" required />
       </div>
       <div className="place-order-right">
         <div className="cart-total">
@@ -49,7 +103,7 @@ const PlaceOrder = () => {
               </b>
             </div>
           </div>
-          <button onClick={() => navigate("/order")}>PROCEED TO PAYMENT</button>
+          <button type="submit" /*onClick={() => navigate("/order")}*/>PROCEED TO PAYMENT</button>
         </div>
       </div>
     </form>
